c1595b2c84c8a922f1d4bea3e00fa70e2248803c
Dmitry Torin <torindev@ukr.net>
2022-07-01 13:18:50 +0300

    upd

--- firebase-get-to-know-flutter/my_own/lib/application_state.dart
+++ firebase-get-to-know-flutter/my_own/lib/application_state.dart
@@ -10,15 +10,43 @@ import 'package:gtk_flutter/src/application_login_state.dart';
 import 'package:gtk_flutter/src/authentication.dart';
 import 'package:provider/provider.dart'; // new
 
+enum Attending { yes, no, unknown }
+
 class ApplicationState extends ChangeNotifier {
   ApplicationState() {
     init();
+  }
+
+  int _attendees = 0;
+  int get attendees => _attendees;
+
+  Attending _attending = Attending.unknown;
+  StreamSubscription<DocumentSnapshot>? _attendingSubscription;
+  Attending get attending => _attending;
+  set attending(Attending attending) {
+    final userDoc = FirebaseFirestore.instance
+        .collection('attendees')
+        .doc(FirebaseAuth.instance.currentUser!.uid);
+    if (attending == Attending.yes) {
+      userDoc.set(<String, dynamic>{'attending': true});
+    } else {
+      userDoc.set(<String, dynamic>{'attending': false});
+    }
   }
 
   Future<void> init() async {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
+
+    FirebaseFirestore.instance
+        .collection('attendees')
+        .where('attending', isEqualTo: true)
+        .snapshots()
+        .listen((snapshot) {
+      _attendees = snapshot.docs.length;
+      notifyListeners();
+    });
 
     FirebaseAuth.instance.userChanges().listen((User? user) {
       if (user != null) {
@@ -37,6 +65,23 @@ class ApplicationState extends ChangeNotifier {
                 message: document.data()['text'] as String,
               ),
             );
+          }
+          notifyListeners();
+        });
+
+        _attendingSubscription = FirebaseFirestore.instance
+            .collection('attendees')
+            .doc(user.uid)
+            .snapshots()
+            .listen((snapshot) {
+          if (snapshot.data() != null) {
+            if (snapshot.data()!['attending'] as bool) {
+              _attending = Attending.yes;
+            } else {
+              _attending = Attending.no;
+            }
+          } else {
+            _attending = Attending.unknown;
           }
           notifyListeners();
         });
--- firebase-get-to-know-flutter/my_own/lib/home_page.dart
+++ firebase-get-to-know-flutter/my_own/lib/home_page.dart
@@ -4,6 +4,7 @@ import 'package:gtk_flutter/guest_book.dart';
 import 'package:gtk_flutter/src/application_login_state.dart';
 import 'package:gtk_flutter/src/authentication.dart';
 import 'package:gtk_flutter/src/widgets.dart';
+import 'package:gtk_flutter/yes_no_selection.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
@@ -48,7 +49,17 @@ class HomePage extends StatelessWidget {
             builder: (context, appState, _) => Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                if (appState.attendees >= 2)
+                  Paragraph('${appState.attendees} people going')
+                else if (appState.attendees == 1)
+                  const Paragraph('1 person going')
+                else
+                  const Paragraph('No one going'),
                 if (appState.loginState == ApplicationLoginState.loggedIn) ...[
+                  YesNoSelection(
+                    state: appState.attending,
+                    onSelection: (attending) => appState.attending = attending,
+                  ),
                   const Header('Discussion'),
                   GuestBook(
                     addMessage: (message) =>
